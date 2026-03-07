47f431f02c24dd3a2dfde43d06041b3adbc5f11b
ahmadstar1221 <49121778+ahmadstar1221@users.noreply.github.com>
2026-03-07 17:03:30 +0100

    Update extern.h

--- MGZ/source/extern.h
+++ MGZ/source/extern.h
@@ -1,4 +1,9 @@
-#include <ppu-types.h>
+#include <stdint.h>
+
+typedef uint8_t  u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
 
 #define printf		 		 	print_load
 #define print_verbose 			print_debug
