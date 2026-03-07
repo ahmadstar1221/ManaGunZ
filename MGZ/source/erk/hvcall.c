c38f5e5c18d9e60b4cd6ac1ae0172e2c31107194
ahmadstar1221 <49121778+ahmadstar1221@users.noreply.github.com>
2026-03-07 13:28:13 +0100

    Update hvcall.c

--- MGZ/source/erk/hvcall.c
+++ MGZ/source/erk/hvcall.c
@@ -47,7 +47,7 @@ int lv1_insert_htab_entry(uint64_t htab_id, uint64_t hpte_group, uint64_t hpte_v
 		"mr %3, %%r6;"
 		: "=r"(result), "=r"(index), "=r"(evicted_v), "=r"(evicted_r)
 		: "r"(htab_id), "r"(hpte_group), "r"(hpte_v), "r"(hpte_r), "r"(bolted_flag), "r"(flags)
-		: "r0", "r2", "r3", "r4", "r5", "r6", "r7", "r8", "r9", "r10", "r11", "r12", "lr", "ctr", "xer", "cr0", "cr1", "cr5", "cr6", "cr7", "memory"
+		: "r0", "r3", "r4", "r5", "r6", "r7", "r8", "r9", "r10", "r11", "r12", "lr", "ctr", "xer", "cr0", "cr1", "cr5", "cr6", "cr7", "memory"
 	);
 		
 	REMOVE_HVSC_REDIRECT();
@@ -76,7 +76,7 @@ int lv1_write_htab_entry(uint64_t vas_id, uint64_t hpte_index, uint64_t hpte_v, 
 		"mr %0, %%r3;"
 		: "=r"(result)
 		: "r"(vas_id), "r"(hpte_index), "r"(hpte_v), "r"(hpte_r)
-		: "r0", "r2", "r3", "r4", "r5", "r6", "r7", "r8", "r9", "r10", "r11", "r12", "lr", "ctr", "xer", "cr0", "cr1", "cr5", "cr6", "cr7", "memory"
+		: "r0", "r3", "r4", "r5", "r6", "r7", "r8", "r9", "r10", "r11", "r12", "lr", "ctr", "xer", "cr0", "cr1", "cr5", "cr6", "cr7", "memory"
 	);
 		
 	REMOVE_HVSC_REDIRECT();
@@ -100,7 +100,7 @@ int lv1_map_memory_area(uint64_t start, uint64_t page_size, uint64_t size, uint6
 		"mr %1, %%r4;"
 		: "=r"(result), "=r"(addr)
 		: "r"(start), "r"(page_size), "r"(size)
-		: "r0", "r2", "r3", "r4", "r5", "r6", "r7", "r8", "r9", "r10", "r11", "r12", "lr", "ctr", "xer", "cr0", "cr1", "cr5", "cr6", "cr7", "memory"
+		: "r0", "r3", "r4", "r5", "r6", "r7", "r8", "r9", "r10", "r11", "r12", "lr", "ctr", "xer", "cr0", "cr1", "cr5", "cr6", "cr7", "memory"
 	);
 	
 	REMOVE_HVSC_REDIRECT();
@@ -119,7 +119,7 @@ int lv1_unmap_memory_area(uint64_t lpar_addr) {
 		SYSCALL(SYSCALL_HVC_REDIRECTOR)
 		:
 		: "r"(lpar_addr)
-		: "r0", "r2", "r3", "r4", "r5", "r6", "r7", "r8", "r9", "r10", "r11", "r12", "lr", "ctr", "xer", "cr0", "cr1", "cr5", "cr6", "cr7", "memory"
+		: "r0", "r3", "r4", "r5", "r6", "r7", "r8", "r9", "r10", "r11", "r12", "lr", "ctr", "xer", "cr0", "cr1", "cr5", "cr6", "cr7", "memory"
 	);
 	
 	REMOVE_HVSC_REDIRECT();
@@ -135,7 +135,7 @@ int lv1_panic(uint64_t flag) {
 		SYSCALL(SYSCALL_HVC_REDIRECTOR)
 		:
 		: "r"(flag)
-		: "r0", "r2", "r3", "r4", "r5", "r6", "r7", "r8", "r9", "r10", "r11", "r12", "lr", "ctr", "xer", "cr0", "cr1", "cr5", "cr6", "cr7", "memory"
+		: "r0", "r3", "r4", "r5", "r6", "r7", "r8", "r9", "r10", "r11", "r12", "lr", "ctr", "xer", "cr0", "cr1", "cr5", "cr6", "cr7", "memory"
 	);
 	
 	REMOVE_HVSC_REDIRECT();
