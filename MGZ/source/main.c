7f289a597db466639b5431ce1d34003026143661
ahmadstar1221 <49121778+ahmadstar1221@users.noreply.github.com>
2026-03-08 03:54:52 +0100

    Update main.c

--- MGZ/source/main.c
+++ MGZ/source/main.c
@@ -16012,10 +16012,21 @@ void add_GAMELIST(char *path)
 	RemoveExtension(title);
 	
 	if(plat == ISO_PS3 || plat == JB_PS3 || plat == ISO_PSP || plat == JB_PSP || plat == BDVD) {
-		if( GetParamSFO("TITLE", title, list_game_path[game_number]) == FAILED) {
-			print_debug("Error : failed to get TITLE from %s", list_game_path[game_number]);	
+
+	if( GetParamSFO("TITLE", title, list_game_path[game_number]) == FAILED
+		|| strstr(title, "Install") != NULL ) {
+
+		/* fallback to PKGDIR */
+		char pkg_path[512];
+		sprintf(pkg_path, "%s/PS3_GAME/PKGDIR", list_game_path[game_number]);
+
+		if(GetParamSFO("TITLE", title, pkg_path) == SUCCESS) {
+			print_debug("Loaded TITLE from PKGDIR for %s", list_game_path[game_number]);
+		} else {
+			print_debug("PKGDIR TITLE not found for %s", list_game_path[game_number]);
 		}
 	}
+}
 	list_game_title[game_number] = strcpy_malloc(title);
 	
 	char ID[20]={0};
